0b992ad2cc1537f931975f5447ea724bb2720246
Sreenath <spsreenath7@gmail.com>
2019-05-11 17:29:23 +0100

    added routing for hacker news API

--- node_lab1/index.js
+++ node_lab1/index.js
@@ -1,7 +1,9 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import contactsRouter from './api/contacts';
+import postsRouter from './api/posts';
 import bodyParser from 'body-parser';
+
 
 dotenv.config();
 
@@ -9,11 +11,12 @@ const app = express();
 
 const port = process.env.PORT;
 
-app.use(express.static('public'));
+//configure body-parser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 
 app.use('/api/contacts', contactsRouter);
+app.use('/api/posts', postsRouter);
 app.use(express.static('public'));
 
 app.listen(port, () => {
